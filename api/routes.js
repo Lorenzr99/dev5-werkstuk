fbd26f1155876367800ca7f5950c104aa0ce49f2
Lorenz Reweghs <lorenz.reweghs@student.ehb.be>
2021-11-06 10:52:39 +0100

    feat(api): add route handlers

--- api/routes.js
+++ api/routes.js
@@ -12,32 +12,34 @@ const pg = require('knex')({
     searchPath: ['knex', 'public'],
 });
 
-festivalRouter.route('/festivals')
-    .get((req,res) => {
-        // TEST: check if PostgreSQL is connected
-        pg.select().from('festivals').then(() => {
-            console.log("PostgreSQL connected");
-        })
-        .catch((e) => {
-            console.log("PostgreSQL not connected");
-            console.error(e);
-        });
-
-        res.send('FESTIVALS');
+const getAllFestivalsHandler = (req,res) => {
+    // TEST: check if PostgreSQL is connected
+    pg.select().from('festivals').then(() => {
+        console.log("PostgreSQL connected");
+    })
+    .catch((e) => {
+        console.log("PostgreSQL not connected");
+        console.error(e);
     });
+    res.send('FESTIVALS');    
+}
+
+const getAllRequestsHandler = (req,res) => {
+    // TEST: check if PostgreSQL is connected
+    pg.select().from('requests').then(() => {
+        console.log("PostgreSQL connected");
+    })
+    .catch((e) => {
+        console.log("PostgreSQL not connected");
+        console.error(e);
+    });
+    res.send('REQUESTS');    
+}
+
+festivalRouter.route('/festivals')
+    .get(getAllFestivalsHandler);
 
 requestRouter.route('/requests')
-    .get((req,res) => {
-        // TEST: check if PostgreSQL is connected
-        pg.select().from('requests').then(() => {
-            console.log("PostgreSQL connected");
-        })
-        .catch((e) => {
-            console.log("PostgreSQL not connected");
-            console.error(e);
-        });
+    .get(getAllRequestsHandler);
 
-        res.send('REQUESTS');
-    });
-
-module.exports = { festivalRouter, requestRouter };
+module.exports = { festivalRouter, requestRouter, getAllFestivalsHandler, getAllRequestsHandler };

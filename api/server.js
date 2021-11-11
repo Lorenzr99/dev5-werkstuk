de927fd6fb8b7fc1a42fcf2f23fd80e197138658
Lorenz Reweghs <lorenz.reweghs@student.ehb.be>
2021-11-11 21:59:51 +0100

    build(api): move server to seperate file

--- api/server.js
+++ api/server.js
@@ -1,12 +1,5 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-const router = require('./routes');
+const app = require('./index');
 
-app.use(express.json());
-app.use('/api', router.festivalRouter);
-app.use('/api', router.requestRouter);
-
-app.listen(port, () => {
-    console.log(`Festivalwijzer API listening at http://localhost:${port}`);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Festivalwijzer API listening at http://localhost:${process.env.PORT || 3000}`);
 });

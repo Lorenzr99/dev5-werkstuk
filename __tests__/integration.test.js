030d0bb97b57781432a4cf9896727e17e7cab650
Lorenz Reweghs <lorenz.reweghs@student.ehb.be>
2021-11-11 21:58:31 +0100

    test(integration): add first test

--- __tests__/integration.test.js
+++ __tests__/integration.test.js
@@ -1,3 +1,11 @@
+const app = require('../api/index.js');
 const supertest = require('supertest');
-const app = require('../api/server.js');
 const request = supertest(app);
+
+describe('festivalRouter', () => {
+    test('GET / endpoint', async (done) => {
+        const res = await request.get('/api/festivals');
+        expect(res.status).toBe(200);
+        return done();
+    })
+})
